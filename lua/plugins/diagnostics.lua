d2848a605c77984117a7397f643831ecfa6631d6
ambaradan <ambaradan@x390.fritz.box>
2024-11-10 21:40:12 +0100

    wip: add diagnostics 'bundles'

---
+++ lua/plugins/diagnostics.lua
@@ -0,0 +1,34 @@
+-- conform.nvim settings
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettier" },
+		html = { "prettier" },
+		sh = { "shfmt" },
+		bash = { "shfmt" },
+		markdown = { "markdownlint" },
+		yaml = { "yamlfmt" },
+	},
+
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+-- nvim-lint.nvim settings
+require("lint").linters_by_ft = {
+	markdown = { "markdownlint", "vale" },
+	yaml = { "yamllint" },
+	bash = { "shellcheck" },
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	callback = function()
+		require("lint").try_lint()
+	end,
+})
+
+-- trouble.nvim settings
+require("trouble").setup()
