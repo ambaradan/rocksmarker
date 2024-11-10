2a8c0c8f525a586d95cde5ae0f93db685b633ec5
ambaradan <ambaradan@x390.fritz.box>
2024-11-10 21:19:19 +0100

    wip: set more mappings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,12 +6,23 @@ local wk = require("which-key")
 wk.add({
 	{ "<leader>n", group = "neo-tree" },
 	{ "<leader>nr", "<cmd>Neotree right toggle<cr>", desc = "Neotree right" },
+	{ "<F12>", "<cmd>Neotree float toggle<cr>", desc = "Neotree float" },
 	{ "<leader>nf", "<cmd>Neotree float toggle<cr>", desc = "Neotree float" },
 	{ "<leader>nc", "<cmd>Neotree git_status bottom<cr>", desc = "Git Status Float", mode = "n" },
 	{ "<leader>d", group = "diagnostics" },
 	{ "<leader>dt", "<cmd>Trouble diagnostics toggle<cr>", desc = "Global Diagnostics" },
 	{ "<leader>db", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics" },
 	{ "<leader>ds", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols" },
+	-- session mappings - persisted.nvim
+	{ "<leader>s", group = "sessions" }, -- group
+	{ "<A-s>", "<cmd>Telescope persisted<cr>", desc = "Select session" },
+	{ "<leader>sS", "<cmd>Telescope persisted<cr>", desc = "Select session" },
+	{ "<leader>ss", "<cmd>SessionSave<cr>", desc = "Save current session", mode = "n" },
+	{ "<leader>sl", "<cmd>SessionLoad<cr>", desc = "Load session", mode = "n" },
+	{ "<leader>st", "<cmd>SessionStop<cr>", desc = "Stop current session", mode = "n" },
+	{ "<A-l>", "<cmd>SessionLoadLast<cr>", desc = "Load last session", mode = "n" },
+
+	-- others mappings
 	{
 		"<leader>F",
 		function()
