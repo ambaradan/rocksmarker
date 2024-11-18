46ac910c4b69d041e80bed71850549b0e980ec50
ambaradan <ambaradan@proton.me>
2024-11-18 12:37:39 +0100

    wip: testing peek

---
+++ lua/plugins/peek.lua
@@ -0,0 +1,5 @@
+require("peek").setup({
+	build = "~/.local/share/rprod/mason/bin/deno task --quiet build:fast",
+	vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {}),
+	vim.api.nvim_create_user_command("PeekClose", require("peek").close, {}),
+})
