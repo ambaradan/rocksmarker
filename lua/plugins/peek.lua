9b9e8e6a48db2e650da2af1b06e95da187324758
ambaradan <ambaradan@proton.me>
2024-11-19 08:37:06 +0100

    fix: remove peek.nvim - need manual setup

--- lua/plugins/peek.lua
+++
@@ -1,5 +0,0 @@
-require("peek").setup({
-	-- build = "~/.local/share/rprod/mason/bin/deno task --quiet build:fast",
-	vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {}),
-	vim.api.nvim_create_user_command("PeekClose", require("peek").close, {}),
-})
