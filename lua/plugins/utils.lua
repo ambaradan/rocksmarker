8d440679a8a13f6ec9375e7227f3ac93a7b9abab
ambaradan <ambaradan@proton.me>
2024-12-11 10:15:54 +0100

    fix: put back 'nvim.autopairs' plugin
    'mini.pairs' Does not properly parify the tag for code blocks,
    the second set of backticks is not recognized.

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -63,8 +63,10 @@ ibl.setup({
 	scope = { enabled = false },
 })
 
--- mini.pairs setting
-require("mini.pairs").setup()
+-- autopairs setup
+require("nvim-autopairs").setup({
+	disable_filetype = { "TelescopePrompt", "vim" },
+})
 
 -- nvim-highlight-colors.nvim settings
 require("nvim-highlight-colors").setup({
