dbfa0abd308305cc8da8202a4594ee5072e2e1be
ambaradan <ambaradan@proton.me>
2024-12-14 18:13:54 +0100

    feat: add telescope.nvim to utils bundle

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,3 +1,54 @@
+-- telescope.nvim settings {{{
+require("telescope").setup({
+	defaults = {
+		layout_strategy = "horizontal",
+		layout_config = {
+			prompt_position = "bottom",
+			horizontal = {
+				height = 0.85,
+			},
+		},
+	},
+	pickers = {
+		buffers = {
+			sort_lastused = true,
+			sort_mru = true,
+			previewer = false,
+			hidden = true,
+			theme = "ivy",
+		},
+		command_history = { theme = "ivy" },
+		git_status = { theme = "ivy" },
+		git_commits = { theme = "ivy" },
+		oldfiles = { previewer = false, theme = "ivy" },
+	},
+	extensions = {
+		file_browser = {
+			theme = "ivy",
+			hide_parent_dir = true,
+			-- disables netrw and use telescope-file-browser in its place
+			hijack_netrw = true,
+			mappings = {
+				["i"] = {
+					-- your custom insert mode mappings
+				},
+				["n"] = {
+					-- your custom normal mode mappings
+				},
+			},
+		},
+		frecency = {
+			show_scores = true,
+		},
+	},
+})
+
+-- in-built pickers
+require("telescope").load_extension("file_browser")
+require("telescope").load_extension("cmdline")
+require("telescope").load_extension("frecency")
+-- }}}
+
 -- persisted.nvim settings {{{
 require("persisted").setup({
 	autoload = false,
