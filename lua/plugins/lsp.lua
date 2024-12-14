3c6d25a33a7d83955da382606a797c15208490a7
ambaradan <ambaradan@proton.me>
2024-12-14 14:28:29 +0100

    feat: add 'luasnip' to lsp bundle

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
+-- nvim-lspconfig settings - LSP capabilities {{{
 -- -- Reserve a space in the gutter
 -- -- This will avoid an annoying layout shift in the screen
 -- vim.opt.signcolumn = "yes"
-
 -- Use LspAttach autocommand to only map the following keys
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -59,7 +59,9 @@ lspconfig.lua_ls.setup({
 		},
 	},
 })
+-- }}}
 
+-- mason and mason-lspconfig settings - ensure_installed servers {{{
 -- IMPORTANT - setting servers to be installed
 -- with mason-lspconfig be done after setting 'capabilities'
 require("mason").setup({})
@@ -73,7 +75,9 @@ require("mason-lspconfig").setup({
 		end,
 	},
 })
--- Additional LSPs for 'nvim-lint' and 'conform'
+-- }}}
+
+-- mason-tool-installer - LSPs for 'nvim-lint' and 'conform' {{{
 require("mason-tool-installer").setup({
 	-- a list of all tools you want to ensure are installed upon
 	-- start
@@ -95,7 +99,9 @@ for _, lsp in ipairs(servers) do
 		capabilities = capabilities,
 	})
 end
+-- }}}
 
+-- nvim-cmp settings - snippets support {{{
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
@@ -181,3 +187,126 @@ cmp.setup({
 		end, { "i", "s" }),
 	},
 })
+-- }}}
+
+-- luasnip.lua settings - markdown snippets {{{
+local ls = require("luasnip")
+local snip = ls.parser.parse_snippet
+
+ls.add_snippets(nil, {
+	all = {},
+	markdown = {
+		-- Headers snippets
+		snip("h1", "# ${1:Enter title header}"),
+		snip("h2", "## ${1:Enter title header}"),
+		snip("h3", "### ${1:Enter title header}"),
+		snip("h4", "#### ${1:Enter title header}"),
+		snip("h5", "##### ${1:Enter title header}"),
+		snip("h6", "######## ${1:Enter title header}"),
+		-- Links snippets
+		snip("l", "[${1:alternate text}](${2})"),
+		snip("link", "[${1:alternate text}](${2})"),
+		-- URLs snippets
+		snip("u", "<${1}> ${0}"),
+		snip("url", "<${1}> ${0}"),
+		-- Image snippet
+		snip("img", "![${1:alt text}](${2:}) ${0}"),
+		-- Text snippet
+		snip("strikethrough", "~~${1}~~ ${0}"),
+		snip("bold", "**${1}** ${0}"),
+		snip("b", "**${1}** ${0}"),
+		snip("i", "*${1}* ${0}"),
+		snip("italic", "*${1}* ${0}"),
+		snip("bold and italic", "***${1}*** ${0}"),
+		snip("bi", "***${1}*** ${0}"),
+		snip("quote", "> ${1}"),
+		-- Code snippets
+		snip("code", "`${1}` ${0}"),
+		snip("codeblock", "```${1:language}\n$0\n```"),
+		-- List snippets
+		snip("unordered list", "- ${1:first}\n- ${2:second}\n- ${3:third}\n$0"),
+		snip("ordered list", "1. ${1:first}\n2. ${2:second}\n3. ${3:third}\n$0"),
+		-- 'mkdocs-material' custom snippets
+		-- Admonitions snippets
+		snip(
+			"material_note",
+			"!!! note ${1:Note title}\n\n\t" .. "${2:Text here - respect the four indentation spaces}"
+		),
+		snip(
+			"material_abstract",
+			'!!! abstract "${1:Abstract title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip("material_info", '!!! info "${1:Info title}"\n\n\t${2:Text here - respect the four indentation spaces}'),
+		snip("material_tip", '!!! tip "${1:Tip title}"\n\n\t${2:Text here - respect the four indentation spaces}'),
+		snip(
+			"material_success",
+			'!!! success "${1:Success title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip(
+			"material_question",
+			'!!! question "${1:Question title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip(
+			"material_warning",
+			'!!! warning "${1:Warning title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip(
+			"material_failure",
+			'!!! failure "${1:Failure title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip(
+			"material_danger",
+			'!!! danger "${1:Danger title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip("material_bug", '!!! bug "${1:Bug title}"\n\n\t${2:Text here - respect the four indentation spaces}'),
+		snip(
+			"material_example",
+			'!!! example "${1:Example title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		snip(
+			"material_quote",
+			'!!! quote "${1:Quote title}"\n\n\t${2:Text here - respect the four indentation spaces}'
+		),
+		-- content tabs
+		snip(
+			"material_content_tab",
+			'=== "${1:Tab one title}"\n\n\t'
+				.. "${2:tab one content here}\n\n"
+				.. '=== "${3:Tab two title}"\n\n\t'
+				.. "${4:tab two content here}\n"
+		),
+		-- Keyboard snippet
+		snip("kbd", "++${0:key}++"),
+		-- Special text snippets
+		snip("sub", "~${1:text}~"),
+		snip("sup", "^${1:text}^"),
+		snip("highlight", "==${0:highlight}=="),
+		snip(
+			"frontmatter",
+			"---\n"
+				.. "title: ${1:Title}\n"
+				.. "author: ${2:author}\n"
+				.. "contributors:\n"
+				.. "tags:\n"
+				.. "    - ${3:tag 1}\n"
+				.. "    - ${4:tag 2}\n"
+				.. "---"
+		),
+		snip("github_note", "> [!NOTE]\n" .. "> ${1:Note here}\n"),
+		snip("github_tip", "> [!TIP]\n" .. "> ${1:Tip here}\n"),
+		snip("github_warning", "> [!WARNING]\n" .. "> ${1:Warning here}\n"),
+		snip("github_caution", "> [!CAUTION]\n" .. "> ${1:Warning here}\n"),
+		-- Task snippets
+		snip("task2", "- [${1| ,x|}] ${2:text}\n- [${3| ,x|}] ${4:text}\n${0}"),
+		snip("task3", "- [${1| ,x|}] ${2:text}\n- [${3| ,x|}] ${4:text}\n- [${5| ,x|}] ${6:text}\n${0}"),
+		snip(
+			"task4",
+			"- [${1| ,x|}] ${2:text}\n- [${3| ,x|}] ${4:text}\n- [${5| ,x|}] ${6:text}\n- [${7| ,x|}] ${8:text}\n${0}"
+		),
+		snip(
+			"task5",
+			"- [${1| ,x|}] ${2:text}\n- [${3| ,x|}] ${4:text}\n- [${5| ,x|}] ${6:text}\n- [${7| ,x|}] ${8:text}\n- [${9| ,x|}] ${10:text}\n${0}"
+		),
+	},
+})
+-- }}}
