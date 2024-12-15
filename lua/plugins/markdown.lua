36e3a7703f342d02fe8944c537749f57284857b5
ambaradan <ambaradan@proton.me>
2024-12-15 13:08:38 +0100

    feat: add 'markdown' bundle

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,3 +1,62 @@
+-- markview.nvim settings {{{
+require("markview").setup({
+	code_blocks = {
+		enable = true,
+		language_direction = "left",
+		sign = false,
+	},
+	links = {
+		enable = true,
+		hyperlinks = {
+			enable = false,
+		},
+	},
+	tables = {
+		enable = false,
+	},
+	headings = {
+		enable = true,
+		shift_width = 1,
+		heading_1 = {
+			style = "icon",
+			icon = " 󰉫 ",
+			sign = " ",
+		},
+		heading_2 = {
+			style = "label",
+			sign = " ",
+			icon = " 󰉬 ",
+			shift_char = " ",
+		},
+		heading_3 = {
+			style = "label",
+			sign = " ",
+			icon = " 󰉭 ",
+			shift_char = " ",
+		},
+		heading_4 = {
+			style = "label",
+			sign = " ",
+			icon = " 󰉮 ",
+			shift_char = " ",
+		},
+		heading_5 = {
+			style = "label",
+			sign = " ",
+			icon = " 󰉯 ",
+			shift_char = " ",
+		},
+		heading_6 = {
+			style = "label",
+			sign = " ",
+			icon = " 󰉰 ",
+			shift_char = " ",
+		},
+	},
+})
+-- }}}
+
+-- markdown.nvim settings {{{
 require("markdown").setup({
 
 	on_attach = function(bufnr)
@@ -11,3 +70,32 @@ require("markdown").setup({
 		vim.keymap.set("x", "<C-s>", toggle("s"), { buffer = bufnr })
 	end,
 })
+-- }}}
+
+-- markdown-table-mode.nvim settings {{{
+require("markdown-table-mode").setup({
+	filetype = {
+		"*.md",
+	},
+	options = {
+		insert = true, -- when typeing "|"
+		insert_leave = true, -- when leaveing insert
+	},
+})
+-- }}}
+
+-- zen-mode.nvim settings {{{
+require("zen-mode").setup({
+	window = {
+		width = 90,
+		options = {
+			number = false,
+			list = true,
+			relativenumber = false,
+		},
+	},
+	plugins = {
+		tmux = { enabled = true },
+	},
+})
+-- }}}
