233e76573875046ca1799cb0cecb14fe7006a6ac
ambaradan <ambaradan@proton.me>
2025-02-19 23:20:38 +0100

    wip: add more material tags

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -9,47 +9,6 @@ vim.wo.spell = true
 vim.bo.spelllang = "en,it"
 vim.bo.spellfile = vim.fn.stdpath("config") .. "/spell/exceptions.utf-8.add"
 
--- local ls = require("luasnip")
--- local snip = ls.parser.parse_snippet
---
--- ls.add_snippets(nil, {
--- 	all = {},
--- 	markdown = {
--- 		-- Headers snippets
--- 		snip("h1", "# ${1:Enter title header}"),
--- 		snip("h2", "## ${1:Enter title header}"),
--- 		snip("h3", "### ${1:Enter title header}"),
--- 		snip("h4", "#### ${1:Enter title header}"),
--- 		snip("h5", "##### ${1:Enter title header}"),
--- 		snip("h6", "######## ${1:Enter title header}"),
--- 		-- Links snippets
--- 		snip("l", "[${1:alternate text}](${2})"),
--- 		snip("link", "[${1:alternate text}](${2})"),
--- 		-- URLs snippets
--- 		snip("u", "<${1}> ${0}"),
--- 		snip("url", "<${1}> ${0}"),
-
--- 		snip("quote", "> ${1}"),
--- 		-- Code snippets
--- 		snip("code", "`${1}` ${0}"),
--- 		snip("codeblock", "```${1:language}\n$0\n```"),
--- 		-- List snippets
--- 		snip("unordered list", "- ${1:first}\n- ${2:second}\n- ${3:third}\n$0"),
--- 		snip("ordered list", "1. ${1:first}\n2. ${2:second}\n3. ${3:third}\n$0"),
---
--- 		-- content tabs
--- 		snip(
--- 			"material_content_tab",
--- 			'=== "${1:Tab one title}"\n\n\t'
--- 				.. "${2:tab one content here}\n\n"
--- 				.. '=== "${3:Tab two title}"\n\n\t'
--- 				.. "${4:tab two content here}\n"
--- 		),
--- 		-- Keyboard snippet
--- 		snip("kbd", "++${0:key}++"),
--- 		-- Special text snippets
--- 		snip("sub", "~${1:text}~"),
--- 		snip("sup", "^${1:text}^"),
 -- 		-- Task snippets
 -- 		snip("task2", "- [${1| ,x|}] ${2:text}\n- [${3| ,x|}] ${4:text}\n${0}"),
 -- 		snip("task3", "- [${1| ,x|}] ${2:text}\n- [${3| ,x|}] ${4:text}\n- [${5| ,x|}] ${6:text}\n${0}"),
@@ -269,6 +228,156 @@ Warning, Failure, Danger, Bug, Example, Quote*
 				i(1, "text"),
 			})
 		),
+		s(
+			{
+				trig = "sub",
+				name = "Material Sub",
+				dscr = "Insert subscripted text in Material format",
+			},
+			fmta("~<1>~", {
+				i(1, "text"),
+			})
+		),
+		s(
+			{
+				trig = "sup",
+				name = "Material Sup",
+				dscr = "Insert superscripted text in Material format",
+			},
+			fmta("^<1>^", {
+				i(1, "text"),
+			})
+		),
+		-- }}}
+		-- material keyboard snippets {{{
+		s(
+			{
+				trig = "kbd",
+				name = "Material Keyboard",
+				dscr = "Insert keyboard tag in Material format",
+			},
+			fmta("++<1>++", {
+				i(1, "key"),
+			})
+		),
+		s(
+			{
+				trig = "kbd",
+				name = "Material Keyboard Expanded",
+				dscr = "Multi keyboard tag in Material format",
+			},
+			fmta('++<1>+"<2>"+"<3>"++', {
+				i(1, "key"),
+				i(2, "letter"),
+				i(3, "letter"),
+			})
+		),
+		-- }}}
+		-- lists snippets {{{
+		s(
+			{
+				trig = "list",
+				name = "Markdown Ordered List",
+				dscr = "Insert a Markdown ordered list",
+			},
+			fmta(
+				[[
+    1. <item1>
+    2. <item2>
+    3. <item3>
+    ]],
+				{
+					item1 = i(1, "First item"),
+					item2 = i(2, "Second item"),
+					item3 = i(3, "Third item"),
+				}
+			)
+		),
+		s(
+			{
+				trig = "list",
+				name = "Markdown Nested Ordered List",
+				dscr = "Insert a nested Markdown ordered list",
+			},
+			fmta(
+				[[
+    1. <item1>
+      1. <subitem1>
+      2. <subitem2>
+    2. <item2>
+      1. <subitem3>
+      2. <subitem4>
+    3. <item3>
+    ]],
+				{
+					item1 = i(1, "First item"),
+					subitem1 = i(2, "Sub-item 1"),
+					subitem2 = i(3, "Sub-item 2"),
+					item2 = i(4, "Second item"),
+					subitem3 = i(5, "Sub-item 3"),
+					subitem4 = i(6, "Sub-item 4"),
+					item3 = i(7, "Third item"),
+				}
+			)
+		),
+		s(
+			{
+				trig = "list",
+				name = "Markdown Unordered List",
+				dscr = "Insert a Markdown unordered list",
+			},
+			fmta(
+				[[
+    - <item1>
+    - <item2>
+    - <item3>
+    ]],
+				{
+					item1 = i(1, "First item"),
+					item2 = i(2, "Second item"),
+					item3 = i(3, "Third item"),
+				}
+			)
+		),
+		s(
+			{
+				trig = "list",
+				name = "Markdown Nested Unordered List",
+				dscr = "Insert a nested Markdown unordered list",
+			},
+			fmta(
+				[[
+    - <item1>
+      - <subitem1>
+      - <subitem2>
+    - <item2>
+      - <subitem3>
+      - <subitem4>
+    - <item3>
+    ]],
+				{
+					item1 = i(1, "First item"),
+					subitem1 = i(2, "Sub-item 1"),
+					subitem2 = i(3, "Sub-item 2"),
+					item2 = i(4, "Second item"),
+					subitem3 = i(5, "Sub-item 3"),
+					subitem4 = i(6, "Sub-item 4"),
+					item3 = i(7, "Third item"),
+				}
+			)
+		),
+		-- }}}
+		-- quote snippet {{{
+		s(
+			{
+				trig = "quote",
+				name = "Markdown Quote",
+				dscr = "Insert a Markdown blockquote",
+			},
+			fmt("> {}", {
+				i(1, "Your quote here"), -- Placeholder for the quote text
+			})
+		),
 		-- }}}
 		-- image snippet {{{
 		s(
@@ -288,7 +397,7 @@ Warning, Failure, Danger, Bug, Example, Quote*
 			)
 		),
 		-- }}}
-		-- inline code snippets {{{
+		-- code snippets {{{
 		s(
 			{
 				trig = "codeblock",
@@ -309,7 +418,7 @@ Warning, Failure, Danger, Bug, Example, Quote*
 		),
 		s(
 			{
-				trig = "incode",
+				trig = "code",
 				name = "Markdown `Inline Code`",
 				dscr = "Insert inline code in Markdown format",
 			},
@@ -318,11 +427,58 @@ Warning, Failure, Danger, Bug, Example, Quote*
 			})
 		),
 		-- }}}
+		-- task snippets {{{
 		s(
 			{
-				trig = "link", -- Trigger for the snippet
-				namr = "Markdown Link", -- Name of the snippet
-				dscr = "Create a Markdown link", -- Description of the snippet
+				trig = "task",
+				name = "Markdown Task List",
+				dscr = "Insert a Markdown task list",
+			},
+			fmta(
+				[[
+    - [ ] <task1>
+    - [ ] <task2>
+    - [ ] <task3>
+    ]],
+				{
+					task1 = i(1, "First task"),
+					task2 = i(2, "Second task"),
+					task3 = i(3, "Third task"),
+				}
+			)
+		),
+		s(
+			{
+				trig = "task",
+				name = "Markdown Nested Task List",
+				dscr = "Insert a Markdown nested task list",
+			},
+			fmta(
+				[[
+- [ ] <main_task1>
+    - [ ] <subtask1_1>
+    - [ ] <subtask1_2>
+- [ ] <main_task2>
+    - [ ] <subtask2_1>
+    - [ ] <subtask2_2>
+            ]],
+				{
+					main_task1 = i(1, "Main Task 1"),
+					subtask1_1 = i(2, "Subtask 1.1"),
+					subtask1_2 = i(3, "Subtask 1.2"),
+					main_task2 = i(4, "Main Task 2"),
+					subtask2_1 = i(5, "Subtask 2.1"),
+					subtask2_2 = i(6, "Subtask 2.2"),
+				}
+			)
+		),
+		-- }}}
+		-- link and url snippets {{{
+		s(
+			{
+				trig = "link",
+				namr = "Markdown Link",
+				dscr = "Create a Markdown link",
 			},
 			fmt(
 				[[
@@ -334,19 +490,64 @@ Warning, Failure, Danger, Bug, Example, Quote*
 				}
 			)
 		),
-		s({
-			trig = "link",
-			namr = "markdown_link",
-			dscr = "Create markdown link [txt](url)",
-		}, {
-			t("["),
-			i(1),
-			t("]("),
-			f(function(_, snip)
-				return snip.env.TM_SELECTED_TEXT[1] or {}
-			end, {}),
-			t(")"),
-			i(0),
-		}),
+		s(
+			{
+				trig = "url",
+				name = "Markdown Bare URL",
+				dscr = "Insert a bare Markdown URL",
+			},
+			fmt("<{}>", {
+				i(1, "http://example.com"),
+			})
+		),
+		-- }}}
+		-- material content tabs {{{
+		s(
+			{
+				trig = "ctabs",
+				name = "MkDocs Material Content Tabs",
+				dscr = "Insert MkDocs Material content tabs",
+			},
+			fmta(
+				[[
+    === "<TabTitle1>"
+
+        <content1>
+    
+    === "<TabTitle2>"
+
+        <content2>
+    ]],
+				{
+					TabTitle1 = i(1, "Tab Title 1"),
+					content1 = i(2, "Content for Tab 1"),
+					TabTitle2 = i(3, "TabTitle2"),
+					content2 = i(4, "Content for Tab 2"),
+				}
+			)
+		),
+		-- }}}
+		-- mermaid snippets {{{
+		s(
+			{
+				trig = "mermaid",
+				name = "Mermaid Diagram",
+				dscr = "Insert a Mermaid diagram template",
+			},
+			fmt(
+				[[
+``` mermaid
+graph LR
+  A%[Start%] --> B{Error?};
+  B -->|Yes| C%[Hmm...%];
+  C --> D%[Debug%];
+  D --> B;
+  B ---->|No| E%[Yay!%];
+```
+    ]],
+				{}
+			)
+		),
+		-- }}}
 	}, -- snippets mark
 })
