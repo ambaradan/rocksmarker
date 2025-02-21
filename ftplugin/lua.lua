418dd7e841b18ab87469a4ce2551acfab237e551
ambaradan <ambaradan@proton.me>
2025-02-21 23:26:59 +0100

    feat: set options for 'lua' in ftplugin

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,3 +1,9 @@
+-- Lua specific settings
+
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldenable = false -- Disable folding at startup.
+
 vim.bo.expandtab = true
 vim.bo.autoindent = true
 vim.bo.tabstop = 2
