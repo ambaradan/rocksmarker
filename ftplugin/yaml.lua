151abff074bd3fe07c8effe758e0afeb34ea20fa
ambaradan <ambaradan@proton.me>
2025-02-21 22:45:29 +0100

    feat: set options for 'yaml' files in ftplugin

--- ftplugin/yaml.lua
+++ ftplugin/yaml.lua
@@ -1,5 +1,13 @@
+-- YAML specific settings
+
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldenable = false -- Disable folding at startup.
+
 vim.bo.expandtab = true
 vim.bo.autoindent = true
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
+
+vim.bo.formatoptions = "cqnj"
