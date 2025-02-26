7e713527e6c82e3706558985745df8695b68c1aa
ambaradan <ambaradan@proton.me>
2025-02-26 23:16:58 +0100

    feat: change folding method

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,11 +1,11 @@
 -- Lua specific settings
 
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.wo.foldenable = false -- Disable folding at startup.
-
 vim.bo.expandtab = true
 vim.bo.autoindent = true
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
+
+vim.wo.foldenable = true -- enable folding
+vim.wo.foldmethod = "marker"
+vim.wo.foldmarker = "{{{,}}}"
