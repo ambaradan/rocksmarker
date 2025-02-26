d08665e50c254f69510d0b4871d5a7e3c01cd02b
ambaradan <ambaradan@proton.me>
2025-02-26 23:15:57 +0100

    wip: testing options

---
+++ ftplugin/asciidoc.lua
@@ -0,0 +1,14 @@
+-- asciidoc.lua
+-- Neovim ftplugin for AsciiDoc files
+vim.bo.commentstring = "// %s"
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.expandtab = true
+vim.wo.number = false
+-- vim.bo.spell = true
+-- vim.wo.wrap = true
+-- vim.cmd("syntax on")
+--
+-- -- Set the 'formatoptions' option to include the 'tcroqn' flags
+-- -- This ensures proper formatting of text, comments, and lists
+-- vim.wo.formatoptions = "tcroqn"
