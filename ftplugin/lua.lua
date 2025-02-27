4b8527f5ed7bf76abc0b390a17457491d82cbf13
ambaradan <ambaradan@proton.me>
2025-02-27 10:10:06 +0100

    fix: set 'spell' off for lua code

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -5,7 +5,7 @@ vim.bo.autoindent = true
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
-
+vim.wo.spell = false
 vim.wo.foldenable = true -- enable folding
 vim.wo.foldmethod = "marker"
 vim.wo.foldmarker = "{{{,}}}"
