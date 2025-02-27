6ae38c5d96d708715c2a8087a54cfd8058ee1994
ambaradan <ambaradan@proton.me>
2025-02-27 09:00:28 +0100

    wip: add 'spell' support to asciidoc

--- ftplugin/asciidoc.lua
+++ ftplugin/asciidoc.lua
@@ -5,10 +5,7 @@ vim.bo.shiftwidth = 2
 vim.bo.tabstop = 2
 vim.bo.expandtab = true
 vim.wo.number = false
--- vim.bo.spell = true
--- vim.wo.wrap = true
--- vim.cmd("syntax on")
---
--- -- Set the 'formatoptions' option to include the 'tcroqn' flags
--- -- This ensures proper formatting of text, comments, and lists
--- vim.wo.formatoptions = "tcroqn"
+
+vim.wo.spell = true
+vim.bo.spelllang = "en,it"
+vim.bo.spellfile = vim.fn.stdpath("config") .. "/spell/exceptions.utf-8.add"
