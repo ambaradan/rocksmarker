4bb471ff27fac5b293fd085f0f1fb97629cd3a6e
ambaradan <ambaradan@proton.me>
2025-02-27 10:36:13 +0100

    chore: comment out some unused local variables

--- snippets/asciidoc.lua
+++ snippets/asciidoc.lua
@@ -2,14 +2,14 @@ require("luasnip.session.snippet_collection").clear_snippets("asciidoc")
 
 local ls = require("luasnip")
 local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
+-- local sn = ls.snippet_node
+-- local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local rep = require("luasnip.extras").rep
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local rep = require("luasnip.extras").rep
 local fmta = require("luasnip.extras.fmt").fmta
-local fmt = require("luasnip.extras.fmt").fmt
+-- local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets(nil, {
 	asciidoc = {
--- snippets/markdown.lua
+++ snippets/markdown.lua
@@ -2,12 +2,12 @@ require("luasnip.session.snippet_collection").clear_snippets("markdown")
 
 local ls = require("luasnip")
 local s = ls.snippet
-local sn = ls.snippet_node
+-- local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node
-local rep = require("luasnip.extras").rep
+-- local f = ls.function_node
+-- local c = ls.choice_node
+-- local rep = require("luasnip.extras").rep
 local fmta = require("luasnip.extras.fmt").fmta
 local fmt = require("luasnip.extras.fmt").fmt
 
