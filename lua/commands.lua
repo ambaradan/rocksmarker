86b7cd212963a6adab21f78d60a1f988ba9b9297
ambaradan <ambaradan@proton.me>
2025-03-06 14:09:19 +0100

    feat: added description to autocommands

--- lua/commands.lua
+++ lua/commands.lua
@@ -3,6 +3,7 @@ local augroup = vim.api.nvim_create_augroup("RocksmarkerSet", { clear = true })
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 	group = augroup,
+	desc = "Reload files when focus is regained or terminal interactions occur",
 	callback = function()
 		if vim.o.buftype ~= "c" then
 			vim.cmd("checktime")
@@ -13,6 +14,7 @@ vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup,
+	desc = "Allow closing specific utility buffers with 'q' key",
 	pattern = {
 		"PlenaryTestPopup",
 		"help",
@@ -33,6 +35,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
 	group = augroup,
+	desc = "Highlight text after yanking to provide visual feedback",
 	callback = function()
 		vim.highlight.on_yank()
 	end,
@@ -41,6 +44,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- vertical help
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup,
+	desc = "Open help documents in a vertical split and equalize window sizes",
 	pattern = "help",
 	callback = function()
 		vim.bo.bufhidden = "unload"
@@ -52,6 +56,7 @@ vim.api.nvim_create_autocmd("FileType", {
 -- no spell for terminal buffer
 vim.api.nvim_create_autocmd({ "TermOpen" }, {
 	group = augroup,
+	desc = "Disable spell checking when opening terminal buffers",
 	callback = function()
 		vim.wo.spell = false
 	end,
