0ff376d4c50e3efa7d6888a4a9d2102d320759b1
ambaradan <ambaradan@proton.me>
2025-03-17 15:50:05 +0100

    feat: add 'numbers' to lua buffer

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -5,6 +5,10 @@ vim.bo.autoindent = true
 vim.bo.tabstop = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth = 2
+
+vim.o.number = true -- enable line numbers
+vim.o.signcolumn = "yes" -- displaying the signs
+
 vim.wo.spell = false
 vim.wo.foldenable = true -- enable folding
 vim.wo.foldmethod = "marker"
