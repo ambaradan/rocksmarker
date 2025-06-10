98349ea0a7dc07c479d0ad33317e145bb513385b
ambaradan <ambaradan@proton.me>
2025-06-10 22:30:50 +0200

    wip: setting 'blink.cmp'

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -148,97 +148,48 @@ for _, lsp in ipairs(servers) do
 end
 -- }}}
 
--- nvim-cmp settings - snippets support {{{
-local cmp = require("cmp")
-local luasnip = require("luasnip")
+-- Autocompletion features - blink.cmo {{{
 
-local select_opts = { behavior = cmp.SelectBehavior.Select }
-
-cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
+require("blink.cmp").setup({
+	keymap = {
+		preset = "default",
+	},
+	completion = {
+		trigger = {
+			-- show_on_trigger_character = true,
+			-- show_on_accept_on_trigger_character = true
+		},
+		menu = {
+			-- auto_show = true,
+			scrollbar = false,
+			draw = {
+				treesitter = { "lsp" },
+			},
+			columns = {
+				{ "kind_icon", "label", gap = 1 },
+				{ "source_name" },
+			},
+			ghost_text = {
+				enabled = true,
+			},
+		},
 	},
 	sources = {
-		{ name = "path" },
-		{ name = "nvim_lsp", keyword_length = 1 },
-		{ name = "buffer", keyword_length = 3 },
-		{ name = "luasnip", keyword_length = 2 },
-		{ name = "lorem_ipsum" },
+		default = { "lsp", "path", "snippets", "buffer" },
 	},
-	window = {
-		documentation = cmp.config.window.bordered(),
-	},
-	formatting = {
-		fields = { "menu", "abbr", "kind" },
-		format = function(entry, item)
-			local menu_icon = {
-				nvim_lsp = "λ",
-				luasnip = "⋗",
-				buffer = "Ω",
-				path = "",
-			}
-
-			item.menu = menu_icon[entry.source.name]
-			return item
-		end,
-	},
-	mapping = {
-		["<Up>"] = cmp.mapping.select_prev_item(select_opts),
-		["<Down>"] = cmp.mapping.select_next_item(select_opts),
-
-		["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
-		["<C-n>"] = cmp.mapping.select_next_item(select_opts),
-
-		["<C-u>"] = cmp.mapping.scroll_docs(-4),
-		["<C-d>"] = cmp.mapping.scroll_docs(4),
-
-		["<C-e>"] = cmp.mapping.abort(),
-		["<C-y>"] = cmp.mapping.confirm({ select = true }),
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-		["<C-f>"] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(1) then
-				luasnip.jump(1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<C-b>"] = cmp.mapping(function(fallback)
-			if luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<Tab>"] = cmp.mapping(function(fallback)
-			local col = vim.fn.col(".") - 1
-
-			if cmp.visible() then
-				cmp.select_next_item(select_opts)
-			elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-				fallback()
-			else
-				cmp.complete()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item(select_opts)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+	cmdline = {
+		keymap = {
+			["Tab"] = { "show", "accept" },
+			["<CR>"] = { "accept_and_enter", "fallback" },
+		},
+		completion = {
+			menu = {
+				auto_show = function(ctx)
+					return vim.fn.getcmdtype() == ":"
+				end,
+			},
+		},
 	},
 })
--- }}}
-
--- luasnip.lua settings - lua loader {{{
-
-require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/snippets" })
 
 -- }}}
