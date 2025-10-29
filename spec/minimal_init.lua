f6199733d7e1bd39a61c745f676e895513b2429e
ambaradan <ambaradan@proton.me>
2025-10-29 12:03:19 +0100

    wip: testing 'rocks init' in minimal_init.lua

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -1,25 +1,63 @@
--- spec/minimal_init.lua
-
--- Set the runtime path for Neovim
+-- Set the runtimepath to include the plugin being tested
 vim.opt.runtimepath:prepend(vim.fn.getcwd())
 
--- Load plenary.nvim for testing
-local plenary_path = vim.fn.stdpath("data") .. "/site/pack/deps/start/plenary.nvim"
-if vim.fn.empty(vim.fn.glob(plenary_path)) > 0 then
-  vim.notify("plenary.nvim not found. Cloning into data directory...")
-  vim.fn.system({
-    "git",
-    "clone",
-    "--depth=1",
-    "https://github.com/nvim-lua/plenary.nvim",
-    plenary_path,
-  })
+-- Load plenary.nvim (required for testing)
+vim.cmd([[runtime plugin/plenary.vim]])
+
+-- Temporary setup for rocks.nvim configuration
+local function setup_rocks_nvim()
+  -- Specifies where to install/use rocks.nvim
+  local install_location = vim.fs.joinpath(vim.fn.stdpath("data"), "rocks")
+
+  -- Set up configuration options related to rocks.nvim
+  local rocks_config = {
+    rocks_path = vim.fs.normalize(install_location),
+  }
+  vim.g.rocks_nvim = rocks_config
+
+  -- Configure the Lua package path to find rocks.nvim plugin code
+  local luarocks_path = {
+    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+    vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+  }
+  package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+  -- Configure the C package path for Lua (e.g., for tree-sitter parsers)
+  local luarocks_cpath = {
+    vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+    vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+  }
+  package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+  -- Add rocks.nvim to the runtimepath
+  vim.opt.runtimepath:append(
+    vim.fs.joinpath(rocks_config.rocks_path, "lib", "luarocks", "rocks-5.1", "rocks.nvim", "*")
+  )
+
+  -- Install rocks.nvim if it is not already installed
+  if not pcall(require, "rocks") then
+    local rocks_location = vim.fs.joinpath(vim.fn.stdpath("cache"), "rocks.nvim")
+    if not vim.uv.fs_stat(rocks_location) then
+      -- Clone rocks.nvim repository
+      local url = "https://github.com/lumen-oss/rocks.nvim"
+      vim.fn.system({ "git", "clone", "--filter=blob:none", url, rocks_location })
+      -- Ensure the clone was successful
+      assert(vim.v.shell_error == 0, "Failed to clone rocks.nvim. Check your connection and try again!")
+    end
+    -- Source the bootstrapping script if the clone was successful
+    vim.cmd.source(vim.fs.joinpath(rocks_location, "bootstrap.lua"))
+    -- Clean up the cloned repository
+    vim.fn.delete(rocks_location, "rf")
+  end
 end
-vim.opt.runtimepath:prepend(plenary_path)
 
--- Load plenary
-require("plenary")
-require("plenary.busted")
+-- Execute the rocks.nvim setup
+setup_rocks_nvim()
 
--- Load helper utilities for tests
-require("spec.helper")
+-- Load the plugin being tested
+require("rocksmarker")
+
+-- Load additional plugin configurations
+require("options")
+require("commands")
+require("mappings")
